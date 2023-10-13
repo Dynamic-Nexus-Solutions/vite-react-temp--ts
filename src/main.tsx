3df8a1e5af15e47a5c71e786762cd79cd3bf3d7d
Anmolnoor <anmolnoor59@gmail.com>
2023-10-14 00:14:56 +0530

    added mantine and react-icons; tailwindcss remved

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import "@mantine/core/styles.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { MantineProvider } from "@mantine/core";
 
 const portal = document.getElementById("root");
 
@@ -10,6 +12,8 @@ if (portal === null) {
 
 ReactDOM.createRoot(portal).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
